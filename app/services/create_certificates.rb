281426cefb38da62a7f515d952dcd312ffb4d83a
Stanislas Boyet <stanislas@boyet.me>
2017-01-18 11:11:34 +0100

    WIP - restore POST & GET

--- app/services/create_certificates.rb
+++ app/services/create_certificates.rb
@@ -30,9 +30,8 @@ class CreateCertificates < ApplicationService
 
     response_body = JSON.parse(response.body)
     certificate = @user.certificates.build(user: @user, app_name: @app_name, domain: @domain, subdomains: @subdomains, debug: @debug)
-    certificate.status_path = response_body['status_path']
-    certificate.identifier = /certificate_generation\/(.*)/.match(certificate.status_path)[1]
-
+    certificate.status_path = response_body['url']
+    certificate.identifier = response_body['uuid']
     if certificate.save
       self.message = 'Certificate saved!'
       self.success = true
@@ -51,8 +50,17 @@ class CreateCertificates < ApplicationService
   API_PATH = ENV['API_PATH']
 
   def send_api_call(app_name, domain, subdomains, debug)
-    raw_uri = "#{API_PATH}/certificate_generation/new/#{domain}?subdomains=#{subdomains}&debug=#{debug}&auth_token=#{auth_token}&app_name=#{app_name}"
-    SendRequest.new(raw_uri).call
+    params = {
+      zone: domain,
+      domains: subdomains,
+      # debug: debug,
+      auth_token: auth_token,
+      heroku_app_name: app_name
+    }
+    raw_uri = "#{API_PATH}/certificate_request"
+    Rails.logger.info "POST to #{raw_uri}"
+    Rails.logger.info "POST for #{params}"
+    SendRequest.new(raw_uri, "POST", params).call
   end
 
   def user_cert_for_app(app_name)
--- app/services/send_request.rb
+++ app/services/send_request.rb
@@ -1,13 +1,38 @@
 class SendRequest
 
-  def initialize(raw_uri)
+  def initialize(raw_uri, verb, params = nil)
     @raw_uri = raw_uri
+    @verb = verb
+    @params = params
   end
 
   def call
     uri = URI.parse(@raw_uri)
     http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Get.new(uri.request_uri)
+    response = make_request_from_http_verb(@verb, uri)
+  end
+
+  private
+
+  def make_request_from_http_verb(verb, uri)
+    case verb
+      when 'GET' then request = make_get_request(uri)
+      when 'POST' then request = make_post_request(uri)
+      else request = make_get_request(uri)
+    end
+    return request
+  end
+
+  def make_get_request(uri)
+    uri.query = URI.encode_www_form(@params) if @params
+    Net::HTTP.get(uri)
+  end
+
+  def make_post_request(uri)
+    http = Net::HTTP.new(uri.host, uri.port)
+    header = {'Content-Type': 'text/json'}
+    request = Net::HTTP::Post.new(uri.request_uri, header)
+    request.body = @params.to_json
     http.request(request)
   end
   
